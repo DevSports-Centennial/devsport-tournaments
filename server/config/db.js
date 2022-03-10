b364a526ecb1dd1076c3a5bf719f16ee1af01982
suvarn badkas <badkassuvarn@gmail.com>
2022-03-10 12:46:38 -0500

    CRUD

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
     //local MongoDB deployment ->
     //"URI": "mongodb://localhost/tournament"
-    "URI": "mongodb+srv://scooby:scoobydooby@cluster0.2mpdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    //"URI": "mongodb+srv://scooby:scoobydooby@cluster0.2mpdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   };
